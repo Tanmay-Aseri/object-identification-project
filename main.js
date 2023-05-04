e3815bef21b8fe8a798e2aceb4338d238c70a587
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-05-04 16:49:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 img ="";
+status = "";
+object = [];
 
 function draw(){
     image(img, 0, 0, 600, 500);
 
-    fill("#FF0000");
-    text("Pan", 190, 60);
-    noFill();
-    stroke("#FF0000");
-    rect(185, 50, 120, 150);
-    fill("#FF0000");
-    text("Plant", 340, 220);
-    noFill();
-    stroke("#FF0000");
-    rect(330, 200, 120, 100);
+    if(status != ""){
+
+      for(i = 0; i < object.length; i++){
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+
+        fill("#FF0000");
+        percent = floor(object[i].confidence*100);
+        text(object[i].label+" "+percent,object[i].x +15, object[i].y+15); 
+        noFill();
+        stroke("#FF0000");
+        rect(object[i].x, object[1].y, object[i].width, object[i].height);  
+      }
+}
 }
 
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function preload(){
   img = loadImage("modern-kitchen.jpg");
+}
+
+function modelLoaded(){
+  console.log("Model Loaded !");
+  status = true;
+  objectDetector.detect(img, gotResult);
+
+}
+
+function gotResult(error, results){
+  if(error){
+    console.log(error);
+
+  }
+
+  console.log(results);
+  object = results;
 }
